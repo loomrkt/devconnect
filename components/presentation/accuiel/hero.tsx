733dcdb1ad2e079c4fa670f95a963d5befcb6423
RAKOTONDRAZAKA Nameno Fanantenana <mrnrakotondrazaka@gmail.com>
2025-03-22 20:45:49 +0300

    erreur zoom chrome

--- components/presentation/accuiel/hero.tsx
+++ components/presentation/accuiel/hero.tsx
@@ -11,7 +11,7 @@ const Hero3 = ({
   description = "DevConnect est un reseau social pour les developpeurs. Partagez vos projets, vos idees et connectez-vous avec d'autres developpeurs",
 }: Hero3Props) => {
   return (
-    <section className="max-w-[1200px] px-4 py-10 sm:py-32 mx-auto">
+    <section className="max-w-[1200px] px-4 py-10 sm:py-16 mx-auto">
       <div className="container grid items-center gap-10 lg:grid-cols-2 lg:gap-20">
         <div className="mx-auto flex flex-col items-center text-center md:ml-auto lg:max-w-3xl lg:items-start lg:text-left">
           <h1 className="my-6 text-4xl font-bold text-pretty lg:text-5xl">
@@ -25,16 +25,15 @@ const Hero3 = ({
           </div>
         </div>
         <div className="relative h-[50svh] lg:h-full overflow-hidden rounded-md">
-            <div className="absolute top-0 left-0 w-[500%] max-w-[45rem] overflow-hidden">
- 
+          <div className="absolute top-0 left-0 w-[500%] max-w-[45rem] overflow-hidden">
             <CldImage
-            width="960"
-            height="600"
-            src="https://res.cloudinary.com/djqniifaa/image/upload/v1742594624/carbon_1_ucqv1p.png"
-            sizes="100vw"
-            alt="Description of my image"
+              width="960"
+              height="600"
+              src="https://res.cloudinary.com/djqniifaa/image/upload/v1742594624/carbon_1_ucqv1p.png"
+              sizes="100vw"
+              alt="Description of my image"
             />
-            </div>
+          </div>
         </div>
       </div>
     </section>
