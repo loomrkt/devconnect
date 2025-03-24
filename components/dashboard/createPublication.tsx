b6e4e341ac6f9f1368b2728084838df343235f3f
RAKOTONDRAZAKA Nameno Fanantenana <mrnrakotondrazaka@gmail.com>
2025-03-24 22:39:19 +0300

    Component definition is missing display name

--- components/dashboard/createPublication.tsx
+++ components/dashboard/createPublication.tsx
@@ -2,28 +2,30 @@ import { auth } from "@/auth";
 import { createClient } from "@/utils/supabase/server";
 import DialogPublication from "./DialogPublication";
 
-export default async () => {
-    const session = await auth();
-    const supabase = await createClient();
-    const { data: user } = await supabase
-      .from("users")
-      .select("*")
-      .eq("email", session!.user!.email)
-      .single();
-    return (
-        <div className="w-full px-4 md:px-8 py-4">
-            <div className="bg-neutral-950 rounded-lg shadow-xs shadow-amber-50/20 p-4">
-                <div className="flex items-center gap-4 w-full">
-                    <img
-                        src={user!.image}
-                        alt="Profile"
-                        className="w-12 h-12 border border-gray-100/30 rounded-full"
-                    />
-                    <div className="w-full">
-                        <DialogPublication user={user} />
-                    </div>
-                </div>
-            </div>
+async function CreatePublication() {
+  const session = await auth();
+  const supabase = await createClient();
+  const { data: user } = await supabase
+    .from("users")
+    .select("*")
+    .eq("email", session!.user!.email)
+    .single();
+  return (
+    <div className="w-full px-4 md:px-8 py-4">
+      <div className="bg-neutral-950 rounded-lg shadow-xs shadow-amber-50/20 p-4">
+        <div className="flex items-center gap-4 w-full">
+          <img
+            src={user!.image}
+            alt="Profile"
+            className="w-12 h-12 border border-gray-100/30 rounded-full"
+          />
+          <div className="w-full">
+            <DialogPublication user={user} />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
+
+export default CreatePublication;
--- components/dashboard/userDrop.tsx
+++ components/dashboard/userDrop.tsx
@@ -19,39 +19,41 @@ interface UserDropProps {
       };
     }
 
-export default ({user}:UserDropProps) => {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-              <div className="relative">
-                <img
-                  src={user.image!}
-                  alt="GitHub profile"
-                  className="w-10 h-10 border-gray-100/30 border rounded-full"
-                />
-                <ChevronDown className="w-4 h-4 absolute bottom-0 right-0 rounded-full bg-neutral-800 border border-gray-100/20" />
-              </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="max-w-56 shadow-lg shadow-amber-50/5">
-              <DropdownMenuLabel>
-                <div className="flex items-center gap-2 py-2">
-                  <Image
-                    width="40"
-                    height="40"
-                    src={user.image!}
-                    alt="GitHub profile"
-                    className="w-10 h-10 border-gray-100/30 border rounded-full"
-                  />
-                  <p>{user.name}</p>
-                </div>
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem variant="destructive">
-                <SignOut />
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-    );
+function UserDrop({ user }: UserDropProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="relative">
+          <img
+            src={user.image!}
+            alt="GitHub profile"
+            className="w-10 h-10 border-gray-100/30 border rounded-full"
+          />
+          <ChevronDown className="w-4 h-4 absolute bottom-0 right-0 rounded-full bg-neutral-800 border border-gray-100/20" />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="max-w-56 shadow-lg shadow-amber-50/5">
+        <DropdownMenuLabel>
+          <div className="flex items-center gap-2 py-2">
+            <Image
+              width="40"
+              height="40"
+              src={user.image!}
+              alt="GitHub profile"
+              className="w-10 h-10 border-gray-100/30 border rounded-full"
+            />
+            <p>{user.name}</p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem variant="destructive">
+          <SignOut />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
+
+export default UserDrop;
